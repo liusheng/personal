992070c2de288c35a3e19d78dd4c97e31921f359
Your Name <you@example.com>
2019-10-23 03:02:39 +0000

    update ss

--- ss_config/install_ss.sh
+++ ss_config/install_ss.sh
@@ -4,4 +4,4 @@ apt update -y
 apt-get install python-pip -y
 pip install shadowsocks 
 
-ssserver -p 10555 -k password -m rc4-md5 --user nobody -d start
+ssserver -p 10555 -k password -m aes-256-cfb --user nobody -d start
